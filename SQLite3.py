2970159be81e65082ef0699642a876b55e4ac441
Jbalderas44839 <balderas_juan@ymail.com>
2022-03-07 10:17:42 -0600

    updated

--- SQLite3.py
+++ SQLite3.py
@@ -7,33 +7,13 @@ try:
     connection = sqlite3.connect('sample')
     cur = connection.cursor()
     print('\nVery good! You have connected to the database successfully!')
+
     #the below is sql code for viewing tables
-    #sql_query = """SELECT name FROM sqlite_master
-    #WHERE type ='table';"""
-    #the below executes the above sql code
-    #cur.execute(sql_query)
+    sql_query = """SELECT name FROM sqlite_master
+    WHERE type ='table';"""
+    cur.execute(sql_query)
 
-    #question4 inserting into table 
-    #=========================================================================
-    #script = """INSERT INTO PRODUCTS
-    #(id, name, inventory, unit)
-    #VALUES
-    #(1234321, 'stapler', 63, '$5.32'),
-    #(2345432, 'calculator', 14, '$83.78') """
-    #cur.execute(script)
-    #print("The records have been inserted successfully into the sample db")
-    #=========================================================================
-    #print("List of tables\n")
-    #the below fetches all the rows of the query 
-    #print(cur.fetchall())
-
-except Exception as e:
-    print("Exception: {}".format(e))
-    raise Exception(e)
-
-#question4 creating table
-#============================================================== 
-#table = """CREATE TABLE PRODUCTS (
+    #table = """CREATE TABLE PRODUCTS (
     #id VARCHAR(255) NOT NULL,
     #name CHAR(255) NOT NULL,
     #inventory VARCHAR(255),
@@ -41,13 +21,28 @@ except Exception as e:
     #);"""
 
     #cur.execute(table)
-
     #print("Table is ready")
-#==============================================================
+    
+    #script = """INSERT INTO PRODUCTS
+    #(id, name, inventory, unit)
+    #VALUES
+    #(1234321, 'stapler', 63, '$5.32'),
+    #(2345432, 'calculator', 14, '$83.78') """
+    #cur.execute(script)
+    #print("The records have been inserted successfully into the sample db")
+
+    print("List of tables\n")
+    #the below fetches all the rows of the query 
+    print(cur.fetchall())
+
+except Exception as e:
+    print("Exception: {}".format(e))
+    raise Exception(e)
+
 #Identifies version of sqlite installed
-#cur.execute('select sqlite_version();')
-#version = cur.fetchall()
-#print('\nThe version of sqlite you have installed is: ',version)
+cur.execute('select sqlite_version();')
+version = cur.fetchall()
+print('\nThe version of sqlite you have installed is: ',version)
 
 #Closes connection to the database
 print('\nTime to disconnect from the database. Please wait...')
